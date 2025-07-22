9ac97e153ab141b6754f62726593247036f8b7a7
ThunderBladeX <magicalpurplehearth@gmail.com>
2025-07-22 14:07:23 +0700

    Create EqModels.kt

---
+++ app/src/main/java/com/alathea/alatheaudio/model/EqModels.kt
@@ -0,0 +1,22 @@
+package com.alathea.alatheaudio.model
+
+data class ParametricBand(
+    val frequency: Float,
+    val gain: Float,
+    val q: Float
+)
+
+sealed class PresetData {
+    data class Graphic(val gains: List<Float>) : PresetData()
+    data class Parametric(val bands: List<ParametricBand>) : PresetData()
+}
+
+data class EqPreset(
+    val name: String,
+    val data: PresetData
+)
+
+enum class EqMode {
+    PARAMETRIC,
+    GRAPHIC
+}
